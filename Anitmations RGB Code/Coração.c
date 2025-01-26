8e81fd222210d8040db391fe030380b79e966453
mirian-nascimento <miriannascimento548@gmail.com>
2025-01-26 14:15:59 -0300

    adicionando valores em hexadecimal das animations e o codigo capaz de trasnformalos em  RGB.

---
+++ Anitmations RGB Code/Coração.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#define NEW_PISKEL_FRAME_COUNT 1
+#define NEW_PISKEL_FRAME_WIDTH 5
+#define NEW_PISKEL_FRAME_HEIGHT 5
+
+/* Piskel data for "New Piskel" */
+
+static const uint32_t new_piskel_data[1][25] = {
+{
+0x00000000, 0xff1e1ecc, 0x00010001, 0xff1e1ecc, 0x00010100, 
+0xff1f1ecd, 0x00010001, 0xff1f1fcc, 0x00010000, 0xff1e1ecd, 
+0xff1e1fcc, 0x00000000, 0x00010000, 0x00000101, 0xff1f1ecc, 
+0x00010000, 0xff1e1fcc, 0x00000101, 0xff1f1ecc, 0x00000100, 
+0x00010100, 0x00000100, 0xff1f1fcd, 0x00010001, 0x00010101
+}
+};
